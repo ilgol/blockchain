be6243578c29221cbe78f65f068d33308b9cfc91
Illia Holovatenko <illia.holovatenko@swanlogic.com>
2018-08-23 18:33:28 +0300

    Added p2p server

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,4 +1,4 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('./index');
 const Block = require('./block');
 
 describe('Blockchain', () => {
@@ -36,5 +36,19 @@ describe('Blockchain', () => {
         bc2.chain[1].data = 'Not foo';
 
         expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('Replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('Does not replace the chain with one of less than or equal to length', () => {
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).not.toEqual(bc2.chain);
     });
 });
